7344581d9150c1d896114842edaf0d52fccdff71
Maxim Rybalkin <mixmax200654@gmail.com>
2021-06-19 22:07:48 +0300

    another bot type

--- main.py
+++ main.py
@@ -1,43 +1,21 @@
-from telegram.ext import Updater
-from telegram.ext import CommandHandler
-from telegram.ext import MessageHandler, Filters
-import requests
+import time
 import os
+from telegram import Bot
 
-TOKEN = os.getenv('TOKEN')
-CHAT_ID = os.getenv('CHAT_ID')
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
 
-updater = Updater(
-    token=TOKEN,
-    use_context=True
-)  # постоянно обновляет/бот, который ждет когда ему что-то напишут, также есть publisher
+def main():
+    bot = Bot(token = BOT_TOKEN)
 
-dispatcher = updater.dispatcher  # понимает, что нужно сделать на определенное действие/какую функцию вызвать на
-# определенное сообщение
+    while True:
+        try:
+            bot.send_message(chat_id = CHAT_ID, text = 'ping from heroku')
 
+        except:
+            print('Ошибка всего')
 
-def start(update, context):  # update - обновляет сообщения, context - хранит бота
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Для получения ссылки на создателя введите /writer")
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Введите город для получения погоды в нем:")
+        time.sleep(20 * 60)
 
-
-def writer(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="https://vk.com/makssk8r")
-
-
-def weather(update, context):
-    city = update.message.text
-    w = requests.get(f'https://wttr.in/{city}?format=4')
-    context.bot.send_message(chat_id=update.effective_chat.id, text=w.text)
-
-
-weather_handler = MessageHandler(Filters.text & (~Filters.command), weather)
-dispatcher.add_handler(weather_handler)
-
-start_handler = CommandHandler('start', start)  # если пользователь напишет старт, то сработает соответсвующая функция
-dispatcher.add_handler(start_handler)  # ну и тут создаем само действие
-writer_handler = CommandHandler('writer', writer)
-dispatcher.add_handler(writer_handler)
-
-updater.start_polling()
-updater.idle()
+if __name__ == '__main__':
+    main()
